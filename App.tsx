2f70dab9b0aaf15bd6336485ae0b4181ef7a01f3
Heitor <heitor.albuquerque@ferreiracosta.com.br>
2024-03-24 20:38:01 -0300

    trabalhando com states

--- App.tsx
+++ App.tsx
@@ -1,61 +1,31 @@
-import { Component } from "react";
-import { Text, View, Image, ImageBackground } from "react-native";
+import { useState } from "react";
+import { Text, View, Image, ImageBackground, Button } from "react-native";
 import Prato from "./components/prato"
 import { StyleSheet } from "react-native";
 import estilos from './estilos/estilos.tsx';
 import Caixa from './components/caixa.tsx';
 import Lista from './components/listaProduto.tsx';
-// export default class App1 extends Component {
+import Mesa from './components/mesa.tsx';
+
+
+
+
 export default function App1() {
+  const [ligado, setLigado] = useState(true);
   const imgbg = './assets/background.jpg';
-  // fexibir = (vp1: any) => {
-  //   if (vp1) {
-  //     return (<Text>APLICATIVO</Text>)
-  //   } else {
-  //     return (<Text> - - - -</Text>)
-  //   }
-  // }
 
-  return (
-
-    <View>
-      <ImageBackground
-        source={require(imgbg)}
-      >
-        <Caixa></Caixa>
-        <View>
-          <Text >Cardápio QR Code</Text>
-          <Text>Menu:</Text>
-          <Prato ingrediente="tomate" preco="1,00" tamanho="médio"></Prato>
-
-          <Lista></Lista>
-          <Image
-            source={require('./assets/app1.png')}
-          />
-        </View>
-      </ImageBackground>
-    </View>
-  );
-
-  //}
-
-
-
-  // const estilos = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: '#ddd',
-  //     alignItems: 'center',
-  //     justifyContent: 'center'
-  //   },
-  //   txt1: {
-  //     color: '#080',
-  //     fontSize: 20,
-  //   },
-
-  // })
 
   const estilos = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ddd',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    txt1: {
+      color: '#080',
+      fontSize: 20,
+    },
     logo: {
       width: 250,
       resizeMode: 'contain'
@@ -65,8 +35,39 @@ export default function App1() {
       resizeMode: "cover",
       width: "100%",
       padding: 10
+    },
+    txtState: {
+      fontSize: 30
     }
   })
 
+  return (
 
+    <View style={estilos.container}>
+      <Mesa nome="mesa1" />
+      {/* <Button
+        title={ligado ? "Desligar" : "Ligar"}
+        onPress={() => setLigado(!ligado)}
+
+      /> */}
+
+
+
+      {/* <ImageBackground
+        source={require(imgbg)}
+      >
+        <Caixa></Caixa>
+        <View>
+          <Text >Cardápio QR Code</Text>
+          <Text>Menu:</Text>
+          <Prato ingrediente="tomate" preco="1,00" tamanho="médio"></Prato>
+          <Text style={estilos.txtState}> * * * *</Text>
+          <Lista></Lista>
+          <Image
+            source={require('./assets/app1.png')}
+          />
+        </View>
+      </ImageBackground> */}
+    </View>
+  );
 }
---
+++ components/mesa.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import { Button, Text, View } from "react-native";
+
+export default class Mesa extends Component {
+    state = {
+        ligado: true
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Mesa:  - Ativa :{this.state.ligado ? 'Sim:' : 'Não'}</Text>
+                <Button
+                    title={this.state.ligado ? 'Desligar' : 'Ligar'}
+                    onPress={
+                        () => { this.setState({ ligado: !this.state.ligado }) }
+                    }
+                />
+            </View>
+        )
+
+    }
+}
